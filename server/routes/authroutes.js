1ec3f5c7f89ba537c0dd488c27535b8b95103c3d
Atharva Nagore <atharvanagore2004@gmail.com>
2024-02-21 19:48:12 +0530

    completed login and register functionality

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
+const authControllers = require("../controllers/auth/authControllers");
 
 const registerSchema = Joi.object({
   username: Joi.string().min(3).max(12).required(),
@@ -13,5 +14,17 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).max(12).required(),
   mail: Joi.string().email().required(),
 });
+
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  authControllers.controllers.postRegister
+);
+router.post(
+  "/login",
+  validator.body(loginSchema),
+  authControllers.controllers.postLogin
+);
+
 
 module.exports = router;
